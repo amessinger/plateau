e8e66cbfcaccd44c89d08747574b8eb06957558e
Antonin Messinger <antonin.messinger@gmail.com>
2020-01-14 17:12:23 +0100

    add socket support

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io').listen(server);
 
 app.use('/assets', express.static(`${__dirname}/assets`));
+app.use('/node_modules', express.static(`${__dirname}/node_modules`));
 
 app.get('/',function(req, res){
   res.sendFile(`${__dirname}/index.html`);
@@ -10,4 +12,10 @@ app.get('/',function(req, res){
 
 server.listen(8080, function() {
   console.log('Listening on ' + server.address().port);
+  io.on('connection', function(socket) {
+    socket.on('hoverCell', function(data) {
+      console.log('hoverCell', data);
+      socket.broadcast.emit('hoverCell', data);
+    });
+  });
 });

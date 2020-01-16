2c5df5f308d4682a64efae9ff826191ae044fd8c
Antonin Messinger <antonin.messinger@gmail.com>
2020-01-16 11:55:39 +0100

    add support for multiple maps

--- server.js
+++ server.js
@@ -3,11 +3,30 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io').listen(server);
 
+const maps = [
+  {
+    image: 'map0.png',
+    cellSize: 32,
+    width: 1088,
+    height: 960
+  },
+  {
+    image: 'map1.png',
+    cellSize: 32,
+    width: 544,
+    height: 416
+  }
+];
+
 app.use('/assets', express.static(`${__dirname}/assets`));
 app.use('/node_modules', express.static(`${__dirname}/node_modules`));
 
 app.get('/',function(req, res){
   res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get('api/maps/:mapId',function({ params }, res){
+  res.send(maps[params.mapId]);
 });
 
 server.listen(8080, function() {
